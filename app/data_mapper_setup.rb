b95eff8638f55c3efcc6e3a010aaef1da033f5ca
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-18 00:44:52 +0000

    database confi

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,12 @@
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+configure :development do
+ DataMapper.setup(:default, "postgresql://#{Dir.pwd}/development.db")
+end
+configure :production do
+ DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 
 require './lib/link'
 require './lib/user'
