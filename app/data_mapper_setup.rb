532adbebfecdd89d08eaed5cef42e26bb4653427
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-18 01:30:09 +0000

    trying

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,8 +2,9 @@ env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
-require './lib/link'
 require './lib/user'
 require './lib/peep'
 
 DataMapper.finalize
+
+DataMapper.auto_upgrade!
